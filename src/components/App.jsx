78f2e1dd7209bf3a94a7bb1c1a42f74b89ff9ea1
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-02-09 21:28:58 -0500

    event on the handleChange

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 function App() {
-
-  function handleChange() {
-    console.log("Changed")
+  function handleChange(event) {
+    console.log(event.target.value);
   }
 
   return (
     <div className="container">
       <h1>Hello </h1>
-      <input onChange={handleChange} type="text" placeholder="What's your name?" />
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
       <button>Submit</button>
     </div>
   );
