2341a89b9410db4c1ca5339dd5a5905abfc4ae93
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-02-09 21:36:41 -0500

    used another useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,25 @@ import React, { useState } from "react";
 
 function App() {
   const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
 
   function handleChange(event) {
     console.log(event.target.value);
     setName(event.target.value);
   }
 
+  function handleClick() {}
+
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
+      <h1>Hello {headingText}</h1>
       <input
         onChange={handleChange}
         type="text"
         placeholder="What's your name?"
         value={name}
       />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
